0d1172fd0f3c4b9600bc98012390eb56ddbdf1ac
CptSoap <cowbear6598@gmail.com>
2023-03-08 00:16:08 +0800

    complete load game scene when game start

---
+++ Assets/Scripts/Main/Bootstrap.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using SoapUtils.SceneSystem;
+using UnityEngine;
+using Zenject;
+
+namespace Main
+{
+    public class Bootstrap : MonoBehaviour
+    {
+        [Inject] private ISceneService sceneService;
+
+        private void Start()
+        {
+            sceneService.DoLoadScene(0);
+        }
+    }
+}
---
+++ Assets/Scripts/Main/MainInstaller.cs
@@ -0,0 +1,15 @@
+﻿using SoapUtils.SceneSystem;
+using Zenject;
+
+namespace Main
+{
+    public class MainInstaller : MonoInstaller<MainInstaller>
+    {
+        public override void InstallBindings()
+        {
+            Container.Bind<LoadHandler>().AsSingle();
+            Container.Bind<StateHandler>().AsSingle();
+            Container.BindInterfacesAndSelfTo<SceneService>().AsSingle();
+        }
+    }
+}
